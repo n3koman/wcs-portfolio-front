3529164fdd905916a0ee4cef4f4461fd83286227
Alisher Izmukhan <alisher.izmukhan@nu.edu.kz>
2024-01-23 19:51:17 +0600

    upd v11

--- ass.js
+++ ass.js
@@ -10,44 +10,16 @@ async function getExistingAssigns() {
   assigns.forEach((assign) => {
     const cardDiv = document.createElement("div");
     cardDiv.className = "col";
-    if (assign.videoContent != '') {
-      const videoId = getYouTubeVideoId(assign.videoContent);
       cardDiv.innerHTML = `
                 <div class="card">
                     <div class="card-body">
                         <span class="card-text">${assign.id}</span>
                         <h5 class="card-title">${assign.title}</h5>
-                        <p class="card-text">${assign.content}</p>
-                        <iframe width="560" height="315" src="https://youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+                        <div class="ql-editor">${assign.content}</div>
                         <p class="card-text"><small class="text-muted">Last updated: ${assign.time}</small></p>
                     </div>
                 </div>`;
-    } else {
-      cardDiv.innerHTML = `
-                <div class="card">
-                    <div class="card-body">
-                        <span class="card-text">${assign.id}</span>
-                        <h5 class="card-title">${assign.title}</h5>
-                        <p class="card-text">${assign.content}</p>
-                        <p class="card-text"><small class="text-muted">Last updated: ${assign.time}</small></p>
-                    </div>
-                </div>`;
-    }
 
-    function getYouTubeVideoId(url) {
-      // Extract video ID from YouTube URL
-      const regExp =
-        /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
-      const match = url.match(regExp);
-
-      if (match && match[2].length === 11) {
-        return match[2];
-      } else {
-        // Handle invalid YouTube URL
-        console.error("Invalid YouTube URL:", url);
-        return "invalid";
-      }
-    }
 
     existingAssignsList.appendChild(cardDiv);
   });
